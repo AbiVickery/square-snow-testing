86d4277147a1dc2b91cbf5913256ffc2ea72e550
Abi Vickery <vickeryabi@gmail.com>
2022-05-24 16:44:39 -0500

    cursor

--- js/app.js
+++ js/app.js
@@ -1,18 +1,3 @@
-// const cursor = document.querySelector('.cursor');
-
-// document.addEventListener('mousemove', e => {
-//     cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px");
-// })
-
-// document.addEventListener('click', () => {
-//     cursor.classList.add("expand");
-
-//     setTimeout(() => {
-//         cursor.classList.remove("expand");
-//     }, 500)
-// })
-
-
 const cursor = document.querySelector('.cursor');
 
 document.addEventListener('mousemove', e => {
@@ -26,3 +11,4 @@ document.addEventListener('click', () => {
         cursor.classList.remove("expand");
     }, 500)
 })
+
